43b97bb5fa9cb6f24980b4b60467ff304c59b4ed
ISAAC <isaaccsugus@gmail.com>
2026-09-21 13:10:03 +0200

    Add files via upload

---
+++ consultas/SQLQuery1.sql
@@ -0,0 +1,6 @@
+ -- Verify key tables in AdventureWorksLT
+ SELECT TOP (5) ProductID, Name, ListPrice 
+ FROM SalesLT.Product;
+    
+ SELECT TOP (5) ProductCategoryID, Name 
+ FROM SalesLT.ProductCategory;
